c6abc1247e1efc8336b7e35edb05e565a940b9ba
Nowwshin <54597147+Nowshin021@users.noreply.github.com>
2021-02-02 22:16:37 +0600

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  double x,y,s;
+  scanf("%lf", &x);
+  scanf("%lf", &y);
+  s = (x/y);
+  printf("%.3lf km/l\n", s);
+
+
+    return 0;
+}
