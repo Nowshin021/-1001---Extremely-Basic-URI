80ef0e2177b357c58bbcecd602a4a770591d7274
Nowwshin <54597147+Nowshin021@users.noreply.github.com>
2021-02-06 00:16:15 +0600

    Create 1158.c

---
+++ 1158.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n,x,y,i,j,sum;
+    scanf("%d" ,&n);
+    for(i=0;i<n;i++)
+    {
+        sum = 0;
+        scanf("%d %d" ,&x,&y);
+        if(x%2==0) x++;
+        for(j=0;j<y;j++)
+        {
+            sum = sum + x;
+            x=x+2;
+        }
+
+        printf("%d\n" , sum);
+    }
+    return 0;
+}
