3b8612fba1cd45490538dd3391a1da6c67b55cf8
Nowwshin <54597147+Nowshin021@users.noreply.github.com>
2021-02-08 00:02:26 +0600

    Create 1924.c

---
+++ 1924.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main()
+{
+   int n,i;
+   char x[100];
+   scanf("%d",&n);
+   for(i=0;i<n;i++){
+    scanf("%s" ,x);
+   }
+
+   printf("Ciencia da Computacao\n");
+    return 0;
+}
